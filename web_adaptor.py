1ba8359fc20742b2e4d7ff1c880113745da578f4
Alejo Martinez <garciamartinezalejo@gmail.com>
2026-07-26 01:41:43 -0300

    Fix: icono

--- web_adaptor.py
+++ web_adaptor.py
@@ -77,7 +77,7 @@ HTML_SUDOKU = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="{{ url_for('static', filename='favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ url_for('static', filename='favicon.ico')}}" type="image/x-icon">
     <title>Visualizador Sudoku Backtracking - Premium Edition</title>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
     <style>
